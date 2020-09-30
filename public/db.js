1ca77e277410fbf72c1d85af153da7cf3f7798dd
Maria <maria.betancourt15@yahoo.com>
2020-09-29 18:58:29 -0700

    added save record function

--- public/db.js
+++ public/db.js
@@ -3,11 +3,30 @@ let db;
 const request = window.indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-  // create object store called "pending" and set autoIncrement to true
+  // create object store "pending"
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
   db = event.target.result;
+  // check if app is online before reading from db
+  //   if (navigator.onLine) {
+  //     checkDatabase();
+  //   }
 };
+
+request.onerror = function (event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+
+  // add record to your store with add method.
+  store.add(record);
+}
