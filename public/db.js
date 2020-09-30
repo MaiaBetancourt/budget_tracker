9f1ee641ec4d320fa9fa8fdba29134e90d9633c7
Maria <maria.betancourt15@yahoo.com>
2020-09-29 18:38:51 -0700

    created the object store called pending

--- public/db.js
+++ public/db.js
@@ -2,7 +2,12 @@ let db;
 // create a new database request
 const request = window.indexedDB.open("budget", 1);
 
+request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
 request.onsuccess = function (event) {
   db = event.target.result;
-
 };
